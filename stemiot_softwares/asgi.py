8f39dd3582a0c140261e2f23443b9784c7fbf68f
stephen-otieno <stephenotieno8781@gmail.com>
2025-06-27 15:28:49 +0300

    initial commit

--- stemiot_softwares/asgi.py
+++ stemiot_softwares/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for stemiot_innovations project.
+ASGI config for stemiot_softwares project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stemiot_innovations.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stemiot_softwares.settings')
 
 application = get_asgi_application()
--- stemiot_softwares/urls.py
+++ stemiot_softwares/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for stemiot_innovations project.
+URL configuration for stemiot_softwares project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
